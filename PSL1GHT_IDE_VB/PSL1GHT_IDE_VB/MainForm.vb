4ed2f2d26a949e3626a54d38cc3ba691689fd9f6
Mauro Javier Montenegro <mauro.montenegro@gmail.com>
2020-06-06 00:44:13 -0300

    Primer comentario del proyecto.

---
+++ PSL1GHT_IDE_VB/PSL1GHT_IDE_VB/MainForm.vb
@@ -0,0 +1,18 @@
+﻿'
+' Creado por SharpDevelop.
+' Usuario: Alumno
+' Fecha: 3 jun. 2020
+' Hora: 12:23
+' 
+' Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+'
+Public Partial Class MainForm
+	Public Sub New()
+		' The Me.InitializeComponent call is required for Windows Forms designer support.
+		Me.InitializeComponent()
+		
+		'
+		' TODO : Add constructor code after InitializeComponents
+		'
+	End Sub
+End Class
